0ec922fe90302dd0def4f5cce64d87b173961a2c
Elismar Silva <elismarsilva.5@outlook.com>
2025-08-22 20:14:44 -0300

    chore(setup): typeorm module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.${process.env.NODE_ENV || 'development'}.env`,
+      envFilePath: ['.development.env', '.env'],
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => databaseConfig(),
     }),
   ],
   controllers: [AppController],
